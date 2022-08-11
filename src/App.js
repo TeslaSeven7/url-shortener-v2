c1980103dc762ad0452705ea8c29d9b0c0e5cdab
TeslaSeven7 <alex.aubrier@epitech.eu>
2022-08-11 19:52:36 +0200

    Expanded on localStorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import { useState, useEffect } from "react";
-let localstore= 'URL'
-let localindex =0;
+let localtab = []
 const url = "https://api.shrtco.de/v2/shorten?url=";
 var longUrl;
+var yolo;
+var obj1= {}
 var regex= /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 export default function App() {
   
@@ -25,11 +26,12 @@ export default function App() {
     }
     setMessage(input);
   };
-  
-  const [isSending, setIsSending] = useState(false);
   const [data, setData] = useState([]);
-  useEffect(() => {
-    if(isSending && typeof longUrl  !== "undefined"){
+  
+  
+  
+  function buttonCheck(){
+    if(typeof longUrl  !== "undefined"){
       if (regex.test(titleinput)){
         fetch(longUrl)
         .then(data => {
@@ -41,11 +43,29 @@ export default function App() {
             return Promise.reject('some other error: ' + data.status)
           }
         }) 
-        .then((data) => setData(data.result.full_short_link))
-        .then(() => setIsSending(false));
+        .then((data) => {
+          setData(data.result.full_short_link);
+          localtab.push(data.result.full_short_link)
+
+
+          
+        })
+        .then(response => {
+          // handle the response
+        })
+        let uniquelocaltab = [];
+        localtab.forEach((c) => {
+          if (!uniquelocaltab.includes(c)) {
+            uniquelocaltab.push(c);
+            localStorage.setItem("keys", uniquelocaltab);
+          }
+        });
+        yolo = localStorage.getItem("keys").split(",");
+        obj1 = Object.assign({}, yolo);
         setMessageTwo('Here is your new URL')
-        setIsSending(false);
-        localindex++;
+        
+    
+        
       }
       else{
         setMessageTwo('This is not a valid URL')
@@ -53,13 +73,44 @@ export default function App() {
       }
       
     }
-    setIsSending(false);
-    
-    
-  }, [isSending]);
-  console.log(localindex)
-  let localkey = localstore + localindex
-  localStorage.setItem(localkey, data);
+  }
+  
+  
+  //const [isSending, setIsSending] = useState(false);
+  
+  // useEffect(() => {
+  //   if(isSending && typeof longUrl  !== "undefined"){
+  //     if (regex.test(titleinput)){
+  //       fetch(longUrl)
+  //       .then(data => {
+  //         if (data.ok) {
+  //           return data.json()
+  //         } else if(data.status === 400) {
+  //           return Promise.reject('error 400')
+  //         } else {
+  //           return Promise.reject('some other error: ' + data.status)
+  //         }
+  //       }) 
+  //       .then((data) => setData(data.result.full_short_link))
+  //       .then(() => setIsSending(false));
+  //       setMessageTwo('Here is your new URL')
+  //       setIsSending(false);
+  //       localindex++;
+  //     }
+  //     else{
+  //       setMessageTwo('This is not a valid URL')
+  //       setData('')
+  //     }
+  
+  //   }
+  //   setIsSending(false);
+  
+  
+  // }, [isSending]);
+  // console.log(localindex)
+  // let localkey = localstore + localindex
+  // localStorage.setItem(localkey, data);
+  //<h3>{localStorage.getItem(localkey, data)}</h3>
   return (
     <>
     <div>
@@ -72,10 +123,11 @@ export default function App() {
     />
     
     <h2>Message: {messageTwo}</h2>
-    <h3>{localStorage.getItem(localkey, data)}</h3>
+    
     </div>
-    <button onClick={() => setIsSending(true)}>Generate URL</button>
-    <h2><a href={data} target="_blank">{data}</a></h2>
+    <button onClick={buttonCheck}>Generate URL</button>
+        <h2><a href={yolo} target="_blank">{yolo}</a></h2>
+    
     
     </>
     );
